939d45de7d3c28d7af801268ca0265a17e31fb87
Emre BUYUKER <emre.buyuker@veripark.com>
2019-12-22 17:53:50 +0300

    Added push notification.

--- Podfile
+++ Podfile
@@ -5,7 +5,9 @@ target 'ViFi' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
+pod 'Firebase/Analytics'
 pod 'Firebase/Core'
+pod 'Firebase/Messaging'
 pod 'Firebase/Database'
 pod 'Firebase/Storage'
 pod 'SDWebImage'
--- ViFi/Application/Delegate/AppDelegate.swift
+++ ViFi/Application/Delegate/AppDelegate.swift
@@ -9,18 +9,42 @@
 import UIKit
 import Firebase
 import GoogleMobileAds
+import FirebaseMessaging
+import UserNotifications
+import FirebaseInstanceID
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        FirebaseApp.configure()
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        return true
+	// The callback to handle data message received via FCM for devices running iOS 10 or above.
+	func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
+		print(remoteMessage.appData)
+	}
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		if #available(iOS 10.0, *) {
+			// For iOS 10 display notification (sent via APNS)
+			UNUserNotificationCenter.current().delegate = self
+			let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+			UNUserNotificationCenter.current().requestAuthorization(
+				options: authOptions,
+				completionHandler: {_, _ in })
+			// For iOS 10 data message (sent via FCM
+			Messaging.messaging().delegate = self
+		} else {
+			let settings: UIUserNotificationSettings =
+				UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+			application.registerUserNotificationSettings(settings)
+		}
+		
+		application.registerForRemoteNotifications()
+		
+		FirebaseApp.configure()
+		GADMobileAds.sharedInstance().start(completionHandler: nil)
+		return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
